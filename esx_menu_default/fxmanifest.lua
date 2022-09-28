5c981fbf7f131032bc365421da92d6693868a037
Leanxp <33395951+Leanxp@users.noreply.github.com>
2022-09-28 19:31:00 +0000

    Add files via upload

---
+++ esx_menu_default/fxmanifest.lua
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+
+
+
+
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'ESX Menu by hamzaonx'
+
+version 'legacy'
+
+client_scripts {
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/logo.png',
+	'html/css/app.css',
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+}
+
+dependencies {
+	'es_extended'
+}
